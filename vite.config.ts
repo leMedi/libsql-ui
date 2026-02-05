ba1e627b5be54df85c1e409680f5fb103385f948
LeMedi <mehdi.elhaij@outlook.com>
2026-02-05 15:59:21 +0100

    shadcn

--- vite.config.ts
+++ vite.config.ts
@@ -5,8 +5,6 @@ import viteReact from '@vitejs/plugin-react'
 import { nitro } from 'nitro/vite'
 import { defineConfig } from 'vite'
 import viteTsConfigPaths from 'vite-tsconfig-paths'
-import { copyFileSync, mkdirSync } from 'node:fs'
-import { join } from 'node:path'
 
 const config = defineConfig({
   plugins: [
@@ -18,20 +16,11 @@ const config = defineConfig({
     }),
     tailwindcss(),
     tanstackStart(),
-    viteReact(),
-    {
-      name: 'copy-native-modules',
-      closeBundle() {
-        const modules = ['@libsql/linux-arm64-musl']
-        modules.forEach((mod) => {
-          const src = join('node_modules', mod)
-          const dest = join('.output/server/node_modules', mod)
-          mkdirSync(dest, { recursive: true })
-          // Copy the module files
-          copyFileSync(join(src, 'index.node'), join(dest, 'index.node'))
-        })
+    viteReact({
+      babel: {
+        plugins: ['babel-plugin-react-compiler'],
       },
-    },
+    }),
   ],
 })
 
